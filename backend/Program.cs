1e5fc5aae1ee14def7d034447efc3dae5f4ed095
luizcamposss <l.gustavosc20@gmail.com>
2026-09-06 18:13:56 -0300

    chore(db): wire pomelo, connection string and empty InitialCreat migration

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,25 @@
+using backend.Shared.Persistence;
 using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not configured");
+}
+
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseMySql(
+        connectionString,
+        new MySqlServerVersion(new Version(8, 4, 0))
+    ));
+
+builder.Services.AddProblemDetails();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,7 +28,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
